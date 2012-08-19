72ec6d02d24ad36ea1e91c9f887960a8fc2b0dcc
Donnchadh O h-Ainle <xhanley@gmail.com>
2012-08-20 00:12:51 +0100

    Flow from home - login - register - feed

--- src/AppMockupJS.js
+++ src/AppMockupJS.js
@@ -1,17 +1,9 @@
-function replace() {
-    var paragraphTag = document.getElementById("para1");
-    console.log("my paragraph is " + paragraphTag)
-     paragraphTag.innerHTML = "new stuff now";
-    //paragraphTag.outerHTML = "<a href='http://google.com'>new stuff now</a>";
-}
-
 function loadScript(){
-
-
-var image = document.getElementById("phoneImage");
-var imageWidth = image.width;
-var imageHeight = image.height;
-console.log("w: %d, h: %d", imageWidth, imageHeight);
+var img = document.createElement('IMG');
+img.setAttribute('id', 'screenImage');
+var screenDiv = document.getElementById("screen");
+screenDiv.appendChild(img);
+loadHomeScreen();
 }
 
 function displayDate()
@@ -30,4 +22,103 @@ document.getElementById('loginButton') = document.getElementById('a').innerHTML;
 function clearField(id)
 {
 document.getElementById(id).value = "";
+}
+
+function loadHomeScreen() 
+{
+if(document.getElementById("userNameField")){
+    cleanUpLoginScreen();
+    }
+var img = document.getElementById("screenImage");
+img.setAttribute('src', '../resources/iosHomeScreen.png');
+img.setAttribute('onclick', 'loadLoginScreen()');
+}
+
+function cleanUpHomeScreen()
+{
+var img = document.getElementById("screenImage");
+img.removeAttribute('onclick');
+}
+
+function loadLoginScreen() 
+{
+if(document.getElementById("userNameFieldR"))
+    cleanUpRegistrationScreen();
+else
+    cleanUpHomeScreen();
+var img = document.getElementById("screenImage");
+img.setAttribute('src', '../resources/loginPageScreen.png');
+createFieldInput('userNameField', 'Username..');
+createFieldInput('passwordField', 'Pass..');
+createButtonInput('loginButton', 'loadExperimentFeedScreen()');
+createButtonInput('registerButton', 'loadRegistrationScreen()');
+createButtonInput('exitButton', 'loadHomeScreen()');
+}
+
+function cleanUpLoginScreen()
+{
+destroyInput('userNameField');
+destroyInput('passwordField');
+destroyInput('loginButton');
+destroyInput('registerButton');
+destroyInput('exitButton');
+}
+
+function loadRegistrationScreen() 
+{
+cleanUpLoginScreen();
+var img = document.getElementById("screenImage");
+img.setAttribute('src', '../resources/registrationPageScreen.png');
+createFieldInput('userNameFieldR', 'Username..');
+createFieldInput('passwordFieldR', 'Pass..');
+createFieldInput('passwordFieldAgainR', 'Pass again..');
+createButtonInput('registerButtonR', 'loadLoginScreen()');
+createButtonInput('backButtonR', 'loadLoginScreen()');
+}
+
+function cleanUpRegistrationScreen()
+{
+destroyInput('userNameFieldR');
+destroyInput('passwordFieldR');
+destroyInput('passwordFieldAgainR');
+destroyInput('registerButtonR');
+destroyInput('backButtonR');
+}
+
+
+function loadExperimentFeedScreen() 
+{
+cleanUpLoginScreen();
+var img = document.getElementById("screenImage");
+img.setAttribute('src', '../resources/experimentFeedScreen.png');
+}
+
+function createFieldInput(id, value)
+{
+var input = document.createElement('INPUT');
+input.setAttribute('id', id);
+input.setAttribute('type', 'text');
+input.setAttribute('value', value);
+var screenDiv = document.getElementById("screen");
+screenDiv.appendChild(input);
+}
+
+function createButtonInput(id, onClickCommand)
+{
+var input = document.createElement('INPUT');
+input.setAttribute('id', id);
+input.setAttribute('onclick', onClickCommand);
+input.setAttribute('type', 'image');
+input.setAttribute('width', 57);
+input.setAttribute('height', 29);
+input.setAttribute('src', '../resources/alphaButton.png');
+var screenDiv = document.getElementById("screen");
+screenDiv.appendChild(input);
+return input;
+}
+
+function destroyInput(id)
+{
+var screenDiv = document.getElementById("screen");
+screenDiv.removeChild(document.getElementById(id));
 }
