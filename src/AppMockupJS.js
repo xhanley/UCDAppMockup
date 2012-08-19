d71359c353d0469d577021c98e560442eb768b26
Donnchadh O h-Ainle <xhanley@gmail.com>
2012-08-19 14:10:22 +0100

    Testing alignments

--- src/AppMockupJS.js
+++ src/AppMockupJS.js
@@ -8,32 +8,3 @@ function replace() {
 function loadScript(){
 alert("Welcome");
 }
-
-
-// in window.onload //document.getElementById("media_device").onchange = 
-function recordScript() {
-    // ready to record
-    audioStream = this.data;
-    recordCtlBut.disabled = false;
-};
-
-function recScript(){
-    if (!recorder) {
-        // start recording
-        recordCtlBut.value = "Stop";
-        recorder = audioStream.record();
-        // set the maximum audio clip length to 10 seconds
-        recordTimer = setTimeout(stopRecording, 10000);
-    } else
-        stopRecording();
-};
-
-function stopRecording() {
-    clearTimeout(recordTimer);
-    var audioFile = recorder.stop();
-    useAudioFile(audioFile);
- 
-    // reset to allow new recording session
-    recorder = null;
-    recordCtlBut.value = "Record";
-}
